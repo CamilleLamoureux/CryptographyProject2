f6f6335c630d919c1c6e54525f497167bfbd19ef
Camille <291756@supinfo.com>
2019-02-24 12:37:47 +0100

    Structure files

---
+++ functions.py
@@ -0,0 +1,42 @@
+def keyOK(n,offset):
+    print("keyOK")
+
+
+def computeCoordonates(n,l,offset):
+    print("computeCoordonates")
+
+
+def init(n,l,coordinates):
+    print("init")
+
+
+def fullDictionaryCipher(text,l,coordinates,dictionary):
+    print("fullDictionaryCipher")
+
+
+def fullDictionaryDecipher(text,n,l,coordinates,dictionary):
+    print("fullDictionaryDecipher")
+
+
+def fullDictionary(text,n,l,coordinates,dictionary,cipher):
+    print("fullDictionary")
+
+
+def displayDictionary(n,l,coordinates,dictionary):
+    print("displayDictionary")
+
+
+def dictionaryToStringCipher(n,l,coordinates,dictionary):
+    print("dictionaryToStringCipher")
+
+
+def dictionaryToStringDecipher(l,coordinates,dictionary):
+    print("dictionaryToStringDecipher")
+
+
+def dictionaryToString(n,l,coordinates,dictionary):
+    print("dictionaryToString")
+
+
+def algorithm2(text,n,offset,cipher,display):
+    print("algorithm2")
